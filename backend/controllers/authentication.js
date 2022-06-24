8fb33d2c529155919823f4fa968170102d9f9f85
Apvivar92@gmail.com <Apvivar92@gmail.com>
2022-06-24 13:06:01 -0700

    checking the user's password using bcrypt

--- backend/controllers/authentication.js
+++ backend/controllers/authentication.js
@@ -8,7 +8,16 @@ router.post('/', async (req, res) => {
   let user = await User.findOne({
     where: { email: req.body.email },
   });
-  console.log(user);
+  if (
+    !user ||
+    !(await bcrypt.compare(req.body.password, user.passwordDigest))
+  ) {
+    res.status(404).json({
+      message: 'Could not find a user with the provided username and password',
+    });
+  } else {
+    res.json({ user });
+  }
 });
 
 module.exports = router;
