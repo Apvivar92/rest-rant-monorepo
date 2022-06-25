25c6006051f642f4789147f7f135174d1c20ccfe
Apvivar92@gmail.com <Apvivar92@gmail.com>
2022-06-25 11:44:05 -0700

    add request handler to the authentication controller

--- backend/controllers/authentication.js
+++ backend/controllers/authentication.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const db = require('../models');
 const bcrypt = require('bcrypt');
+const { TimeoutError } = require('sequelize/types');
 
 const { User } = db;
 
@@ -17,6 +18,17 @@ router.post('/', async (req, res) => {
     });
   } else {
     res.json({ user });
+  }
+});
+
+router.get('/profile', async (req, res) => {
+  try {
+    let user = await User.findOne({
+      where: { userId: id },
+    });
+    res.json(user);
+  } catch {
+    res.json(null);
   }
 });
 
